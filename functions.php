43f299452de4900309315ee95a03502978dab4e7
otilik <otiliaghik@gmail.com>
2023-07-12 17:35:33 +0300

    about & author pages

--- functions.php
+++ functions.php
@@ -294,13 +294,13 @@ function add_read_more_link($content) {
   function author_users_shortcode() {
     $users = get_users( array( 'role' => 'author' ) );
 
-    $output = '<ul>';
+    $output = '<ul class="authors-list">';
 
     foreach ( $users as $user ) {
         $user_profile_url = get_author_posts_url( $user->ID );
         $user_profile_picture = get_avatar( $user->ID, 32 );
 
-        $output .= '<li><a href="' . $user_profile_url . '">' . $user_profile_picture . ' ' . $user->display_name . '</a></li>';
+        $output .= '<li><a href="' . $user_profile_url . '">' . $user_profile_picture . '<div class="author-box"><h5>Author</h5> <span class="author-name">' . $user->display_name . '</span></div></a></li>';
     }
 
     $output .= '</ul>';
@@ -313,27 +313,31 @@ add_shortcode( 'author_users', 'author_users_shortcode' );
 function contributor_users_shortcode() {
     $users = get_users( array( 'role' => 'contributor' ) );
 
-    $output = '<ul>';
+    $output = '<ul class="contributors">';
 
     foreach ( $users as $user ) {
         $user_profile_url = get_author_posts_url( $user->ID );
         $user_contact_info = '';
 
         // Check if contact information is available
-        if ( ! empty( $user->linkedin ) ) {
-            $user_contact_info .= '<a href="' . $user->linkedin . '">Linkedin</a> ';
-        }
-        if ( ! empty( $user->facebook ) ) {
-            $user_contact_info .= '<a href="' . $user->facebook . '">Facebook</a> ';
+               if ( ! empty( $user->facebook ) ) {
+            $user_contact_info .= '<a class="fb" href="' . $user->facebook . '"><svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19.6875 10.5C19.6875 15.3438 16.1328 19.3672 11.4844 20.0703V13.3125H13.75L14.1797 10.5H11.4844V8.70312C11.4844 7.92188 11.875 7.17969 13.0859 7.17969H14.2969V4.79688C14.2969 4.79688 13.2031 4.60156 12.1094 4.60156C9.92188 4.60156 8.47656 5.96875 8.47656 8.39062V10.5H6.01562V13.3125H8.47656V20.0703C3.82812 19.3672 0.3125 15.3438 0.3125 10.5C0.3125 5.14844 4.64844 0.8125 10 0.8125C15.3516 0.8125 19.6875 5.14844 19.6875 10.5Z" fill="#0F3548"/></svg></a> ';
         }
         if ( ! empty( $user->instagram ) ) {
-            $user_contact_info .= '<a href="' . $user->instagram . '">Instagram</a> ';
+            $user_contact_info .= '<a class="insta" href="' . $user->instagram . '"><svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9.75 5.00781C12.2109 5.00781 14.2422 7.03906 14.2422 9.5C14.2422 12 12.2109 13.9922 9.75 13.9922C7.25 13.9922 5.25781 12 5.25781 9.5C5.25781 7.03906 7.25 5.00781 9.75 5.00781ZM9.75 12.4297C11.3516 12.4297 12.6406 11.1406 12.6406 9.5C12.6406 7.89844 11.3516 6.60938 9.75 6.60938C8.10938 6.60938 6.82031 7.89844 6.82031 9.5C6.82031 11.1406 8.14844 12.4297 9.75 12.4297ZM15.4531 4.85156C15.4531 4.26562 14.9844 3.79688 14.3984 3.79688C13.8125 3.79688 13.3438 4.26562 13.3438 4.85156C13.3438 5.4375 13.8125 5.90625 14.3984 5.90625C14.9844 5.90625 15.4531 5.4375 15.4531 4.85156ZM18.4219 5.90625C18.5 7.35156 18.5 11.6875 18.4219 13.1328C18.3438 14.5391 18.0312 15.75 17.0156 16.8047C16 17.8203 14.75 18.1328 13.3438 18.2109C11.8984 18.2891 7.5625 18.2891 6.11719 18.2109C4.71094 18.1328 3.5 17.8203 2.44531 16.8047C1.42969 15.75 1.11719 14.5391 1.03906 13.1328C0.960938 11.6875 0.960938 7.35156 1.03906 5.90625C1.11719 4.5 1.42969 3.25 2.44531 2.23438C3.5 1.21875 4.71094 0.90625 6.11719 0.828125C7.5625 0.75 11.8984 0.75 13.3438 0.828125C14.75 0.90625 16 1.21875 17.0156 2.23438C18.0312 3.25 18.3438 4.5 18.4219 5.90625ZM16.5469 14.6562C17.0156 13.5234 16.8984 10.7891 16.8984 9.5C16.8984 8.25 17.0156 5.51562 16.5469 4.34375C16.2344 3.60156 15.6484 2.97656 14.9062 2.70312C13.7344 2.23438 11 2.35156 9.75 2.35156C8.46094 2.35156 5.72656 2.23438 4.59375 2.70312C3.8125 3.01562 3.22656 3.60156 2.91406 4.34375C2.44531 5.51562 2.5625 8.25 2.5625 9.5C2.5625 10.7891 2.44531 13.5234 2.91406 14.6562C3.22656 15.4375 3.8125 16.0234 4.59375 16.3359C5.72656 16.8047 8.46094 16.6875 9.75 16.6875C11 16.6875 13.7344 16.8047 14.9062 16.3359C15.6484 16.0234 16.2734 15.4375 16.5469 14.6562Z" fill="#0F3548"/></svg></a> ';
         }
         if ( ! empty( $user->twitter ) ) {
-            $user_contact_info .= '<a href="' . $user->twitter . '">Twitter</a> ';
+            $user_contact_info .= '<a class="tweet" href="' . $user->twitter . '"><svg width="20" height="17" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.9297 4.4375C17.9297 4.63281 17.9297 4.78906 17.9297 4.98438C17.9297 10.4141 13.8281 16.625 6.28906 16.625C3.94531 16.625 1.79688 15.9609 0 14.7891C0.3125 14.8281 0.625 14.8672 0.976562 14.8672C2.89062 14.8672 4.64844 14.2031 6.05469 13.1094C4.25781 13.0703 2.73438 11.8984 2.22656 10.2578C2.5 10.2969 2.73438 10.3359 3.00781 10.3359C3.35938 10.3359 3.75 10.2578 4.0625 10.1797C2.1875 9.78906 0.78125 8.14844 0.78125 6.15625V6.11719C1.32812 6.42969 1.99219 6.58594 2.65625 6.625C1.52344 5.88281 0.820312 4.63281 0.820312 3.22656C0.820312 2.44531 1.01562 1.74219 1.36719 1.15625C3.39844 3.61719 6.44531 5.25781 9.84375 5.45312C9.76562 5.14062 9.72656 4.82812 9.72656 4.51562C9.72656 2.25 11.5625 0.414062 13.8281 0.414062C15 0.414062 16.0547 0.882812 16.8359 1.70312C17.7344 1.50781 18.6328 1.15625 19.4141 0.6875C19.1016 1.66406 18.4766 2.44531 17.6172 2.95312C18.4375 2.875 19.2578 2.64062 19.9609 2.32812C19.4141 3.14844 18.7109 3.85156 17.9297 4.4375Z" fill="#0F3548"/></svg></a> ';
+        }
+        if ( ! empty( $user->linkedin ) ) {
+            $user_contact_info .= '<a class="linkd" href="' . $user->linkedin . '"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.875 0.625C18.6055 0.625 19.25 1.26953 19.25 2.04297V18.5C19.25 19.2734 18.6055 19.875 17.875 19.875H1.33203C0.601562 19.875 0 19.2734 0 18.5V2.04297C0 1.26953 0.601562 0.625 1.33203 0.625H17.875ZM5.80078 17.125V7.97266H2.96484V17.125H5.80078ZM4.38281 6.68359C5.28516 6.68359 6.01562 5.95312 6.01562 5.05078C6.01562 4.14844 5.28516 3.375 4.38281 3.375C3.4375 3.375 2.70703 4.14844 2.70703 5.05078C2.70703 5.95312 3.4375 6.68359 4.38281 6.68359ZM16.5 17.125V12.0977C16.5 9.64844 15.9414 7.71484 13.0625 7.71484C11.6875 7.71484 10.7422 8.48828 10.3555 9.21875H10.3125V7.97266H7.60547V17.125H10.4414V12.6133C10.4414 11.4102 10.6562 10.25 12.1602 10.25C13.6211 10.25 13.6211 11.625 13.6211 12.6562V17.125H16.5Z" fill="#0F3548"/></svg></a> ';
         }
 
-        $output .= '<li><a href="' . $user_profile_url . '">' . $user->display_name . '</a>' . ($user_contact_info ? ' - ' . $user_contact_info : '') . '</li>';
+        $output .= '<li><a class="name-contributor" href="' . $user_profile_url . '">' . $user->display_name . '</a><div class="contributor-info">' . ($user_contact_info ? ' ' . $user_contact_info : '') . '</div></li>';
     }
 
     $output .= '</ul>';
@@ -451,3 +455,100 @@ function books_by_author_shortcode() {
 }
 add_shortcode('books_by_author', 'books_by_author_shortcode');
 
+// Custom shortcode to display author's posts with featured image, category, and title
+function author_posts_shortcode() {
+    // Retrieve author's ID of the current page
+    $author_id = get_queried_object_id();
+
+    // Retrieve author's posts based on the author ID
+    $args = array(
+        'author'         => $author_id,
+        'post_type'      => 'post',
+        'posts_per_page' => -1,
+    );
+
+    // Query the posts
+    $query = new WP_Query($args);
+
+    // Start output buffering
+    ob_start();
+
+    // Display posts
+    if ($query->have_posts()) { ?>
+    <h2 class="heading-articles">Author articles</h2>
+    <div class="author-articles">
+     <?php  while ($query->have_posts()) {
+            $query->the_post();
+            ?>
+            <div class="author-post">
+                <?php if (has_post_thumbnail()) : ?>
+                    <div class="featured-image">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('full'); ?>
+                        </a>
+                    </div>
+                <?php endif; ?>
+                <div class="post-details">
+                    <span class="post-category"><?php the_category(', '); ?></span>
+                    <span class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span>
+                </div>
+            </div>
+            <?php
+        } ?>
+        </div>
+        <?php } 
+    // Restore original post data
+    wp_reset_postdata();
+
+    // End output buffering and return content
+    return ob_get_clean();
+}
+add_shortcode('author_posts', 'author_posts_shortcode');
+
+
+// Custom shortcode to display other authors on author page
+function other_authors_shortcode() {
+    $current_author_id = get_queried_object_id();
+
+    // Retrieve other authors with the "Author" role
+    $args = array(
+        'role'    => 'author',
+        'exclude' => $current_author_id,
+        'number'  => 3,
+    );
+
+    $authors = get_users($args);
+
+    // Start output buffering
+    ob_start();
+
+    // Display other authors
+    if (!empty($authors)) {
+        ?>
+        <h2>See other authors</h2>
+        <div class="others-container">
+        <?php
+        foreach ($authors as $author) {
+            $author_name = $author->display_name;
+            $author_id   = $author->ID;
+            $author_url  = get_author_posts_url($author_id);
+            $author_avatar = get_avatar($author_id, 96);
+
+            ?>
+            <div class="author-card">
+                <a href="<?php echo $author_url; ?>">
+                    <?php echo $author_avatar; ?>
+                    <div class="author-box"><h5>Author</h5> 
+                        <span class="author-name"><?php echo $author_name; ?></span>
+                    </div>
+                </a>
+            </div>
+            <?php
+        } ?>
+        </div>
+        <?php } 
+
+    // End output buffering and return content
+    return ob_get_clean();
+}
+add_shortcode('other_authors', 'other_authors_shortcode');
